7f0c9b3f5e2a15bc468e682b6360c5b5d17406e5
Sardor <sardorabdurasulov2oo1@gmail.com>
2024-05-10 23:55:34 +0500

    added js file

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,19 @@
+// HEADER LINKS
+const headerLinks = document.querySelectorAll(".header__link")
+
+headerLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    headerLinks.forEach(link => link.classList.remove("header__link--active"))
+    link.classList.add("header__link--active")
+  })
+})
+
 // LOGIN
 const loginBtn = document.querySelector(".nav__login")
 const login = document.querySelector(".login")
 const loginClose = document.getElementById("login-btn")
+const loginInput = document.getElementById("login-password")
+const loginEyes = document.querySelectorAll(".login__eye")
 
 loginBtn.addEventListener("click", () => {
   login.classList.add("login-show")
@@ -14,11 +26,6 @@ login.addEventListener("click", (e) => {
 loginClose.addEventListener("click", () => {
   login.classList.remove("login-show")
 })
-
-// INPUT PASSWORD
-
-const loginInput = document.getElementById("login-password")
-const loginEyes = document.querySelectorAll(".login__eye")
 
 loginEyes.forEach(button => {
   button.addEventListener("click", () => {
