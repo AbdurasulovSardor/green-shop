d00b66e0e3fd28b604a65b8c99996d2821f4cdfc
Sardor <sardorabdurasulov2oo1@gmail.com>
2024-05-12 05:22:27 +0500

    added hero carousel

--- src/js/script.js
+++ src/js/script.js
@@ -40,3 +40,71 @@ loginEyes.forEach(button => {
     }
   })
 })
+
+// HERO 
+const heroContainer = document.querySelector(".hero__container")
+const heroList = document.querySelector(".hero__list")
+const firstItemWidth = heroList.querySelector(".hero__item").offsetWidth
+const heroListChildren = [...heroList.children]
+let isDragging = false, startX, startScrollLeft, timeoutId
+
+let itemPerView = Math.round(heroList.offsetWidth / firstItemWidth)
+
+heroListChildren.slice(-itemPerView).reverse().forEach(card => {
+  heroList.insertAdjacentHTML("afterbegin", card.outerHTML)
+})
+
+heroListChildren.slice(0, itemPerView).forEach(card => {
+  heroList.insertAdjacentHTML("beforeend", card.outerHTML)
+})
+
+const dragStart = (e) => {
+  isDragging = true
+  heroList.classList.add("dragging")
+  startX = e.pageX || e.touches[0].pageX
+  startScrollLeft = heroList.scrollLeft
+}
+
+const dragging = e => {
+  if (!isDragging) return
+  heroList.scrollLeft = startScrollLeft - (e.pageX - startX) || (e.touches[0].pageX - startX)
+}
+
+const dragStop = () => {
+  isDragging = false
+  heroList.classList.remove("dragging")
+}
+
+const autoPlay = () => {
+  timeoutId = setTimeout(() => heroList.scrollLeft += firstItemWidth, 2000);
+}
+
+autoPlay()
+
+const infiniteScroll = () => {
+  if (heroList.scrollLeft === 0) {
+    heroList.classList.add("no-transition")
+    heroList.scrollLeft = heroList.scrollWidth - (2 * heroList.offsetWidth)
+    heroList.classList.remove("no-transition")
+  } else if (Math.ceil(heroList.scrollLeft) === heroList.scrollWidth - heroList.offsetWidth) {
+    heroList.classList.add("no-transition")
+    heroList.scrollLeft = heroList.offsetWidth
+    heroList.classList.remove("no-transition")
+  }
+
+  clearTimeout(timeoutId)
+  if (!heroContainer.matches(":hover")) autoPlay()
+}
+
+heroList.addEventListener("mousedown", dragStart)
+heroList.addEventListener("touchstart", dragStart)
+
+heroList.addEventListener("mousemove", dragging)
+heroList.addEventListener("touchmove", dragging)
+
+document.addEventListener("mouseup", dragStop)
+
+heroList.addEventListener("scroll", infiniteScroll)
+heroContainer.addEventListener("mouseenter", () => clearTimeout(timeoutId))
+heroContainer.addEventListener("mouseleave", autoPlay)
+heroContainer.addEventListener("touchend", autoPlay)
