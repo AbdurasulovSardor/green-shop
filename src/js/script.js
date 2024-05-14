fa495d4581a867eeee0f6c66f2e2269bf7d384bd
Sardor <sardorabdurasulov2oo1@gmail.com>
2024-05-14 07:13:18 +0500

    added cards js

--- src/js/script.js
+++ src/js/script.js
@@ -108,3 +108,31 @@ heroList.addEventListener("scroll", infiniteScroll)
 heroContainer.addEventListener("mouseenter", () => clearTimeout(timeoutId))
 heroContainer.addEventListener("mouseleave", autoPlay)
 heroContainer.addEventListener("touchend", autoPlay)
+
+// PRICE RANGE
+
+const rangeInput = document.querySelectorAll(".catalog__range-input input")
+const progress = document.querySelector(".catalog__progress")
+const priceText = document.querySelectorAll(".catalog__price-box p")
+
+let priceGap = 1000
+
+rangeInput.forEach(input => {
+  input.addEventListener("input", (e) => {
+    let minVal = parseInt(rangeInput[0].value),
+      maxVal = parseInt(rangeInput[1].value)
+
+    if (maxVal - minVal < priceGap) {
+      if (e.target.className === "catalog__range-min") {
+        rangeInput[0].value = maxVal - priceGap
+      } else {
+        rangeInput[1].value = minVal + priceGap
+      }
+    } else {
+      priceText[0].innerHTML = "$" + minVal
+      priceText[1].innerHTML = "$" + maxVal
+      progress.style.left = (minVal / rangeInput[0].max) * 100 + "%"
+      progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%"
+    }
+  })
+})
